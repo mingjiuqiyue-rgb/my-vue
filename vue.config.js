e72cc33de57f342c559f6e306a3c6e2d34e9bcb7
mingjiuqiyue-rgb <mingjiuqiyue@gmail.com>
2026-02-03 09:41:08 +0800

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/repository-name/" // GitHub 仓库名
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/my-vue/" : "/",
 });
